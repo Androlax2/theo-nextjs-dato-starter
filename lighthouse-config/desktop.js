62abadeee8c9755c9201e735e09c7a4a69262283
Théo Benoit <39646949+Androlax2@users.noreply.github.com>
2025-05-21 14:56:23 -0400

    Update desktop.js

--- lighthouse-config/desktop.js
+++ lighthouse-config/desktop.js
@@ -10,10 +10,10 @@ module.exports = deepmerge(commonConfig, {
     },
     assert: {
       assertions: {
-        "categories:performance": ["error", { minScore: 0.95 }],
-        "categories:accessibility": ["error", { minScore: 1.0 }],
-        "categories:best-practices": ["error", { minScore: 1.0 }],
-        "categories:seo": ["error", { minScore: 1.0 }],
+        "categories:performance": ["error", { minScore: 0.9 }],
+        "categories:accessibility": ["error", { minScore: 0.9 }],
+        "categories:best-practices": ["error", { minScore: 0.9 }],
+        "categories:seo": ["error", { minScore: 0.9 }],
       },
     },
   },
