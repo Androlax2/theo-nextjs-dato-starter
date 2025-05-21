5e6fc705682bcfd0960ea0363ba2455a9586e40c
Théo Benoit <39646949+Androlax2@users.noreply.github.com>
2025-05-21 14:56:12 -0400

    Update mobile.js

--- lighthouse-config/mobile.js
+++ lighthouse-config/mobile.js
@@ -11,9 +11,9 @@ module.exports = deepmerge(commonConfig, {
     assert: {
       assertions: {
         "categories:performance": ["error", { minScore: 0.6 }],
-        "categories:accessibility": ["error", { minScore: 0.95 }],
-        "categories:best-practices": ["error", { minScore: 1.0 }],
-        "categories:seo": ["error", { minScore: 1.0 }],
+        "categories:accessibility": ["error", { minScore: 0.9 }],
+        "categories:best-practices": ["error", { minScore: 0.9 }],
+        "categories:seo": ["error", { minScore: 0.9 }],
       },
     },
   },
